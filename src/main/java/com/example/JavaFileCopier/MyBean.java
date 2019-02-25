fe17c4bdd27fbe38fbac81fc2b33296c192b725f
milind.technopreneur@gmail.com <milind.technopreneur@gmail.com>
2019-02-26 02:44:44 +0530

    Add

---
+++ src/main/java/com/example/JavaFileCopier/MyBean.java
@@ -0,0 +1,16 @@
+package com.example.JavaFileCopier;
+
+public class MyBean {
+
+	public String test(String name) {
+		String result = "Hello " + name;
+		System.out.println(result);
+		return result;
+	}
+
+	public String test2(String name) {
+		System.out.println("Hello2 "+name);
+		return "Hello";
+	}
+
+}
